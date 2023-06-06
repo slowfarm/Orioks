06d5c501c9e42154f011301251ba2e678d84cb06
Vladimir40a <altair08111994@yandex.ru>
2023-06-06 14:24:30 +0300

    initial commit

--- app/src/main/java/ru/eva/oriokslive/ui/activity/news/NewsActivity.kt
+++ app/src/main/java/ru/eva/oriokslive/ui/activity/news/NewsActivity.kt
@@ -1,23 +1,37 @@
 package ru.eva.oriokslive.ui.activity.news
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.MenuItem
+import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import ru.eva.oriokslive.databinding.ActivityNewsBinding
 import ru.eva.oriokslive.ui.base.BaseActivity
 import ru.eva.oriokslive.ui.fragment.news.NewsFragment.Companion.EXTRA_URL
 
+
+@AndroidEntryPoint
 class NewsActivity : BaseActivity<ActivityNewsBinding>() {
     override val bindingInflater: (LayoutInflater) -> ActivityNewsBinding =
         ActivityNewsBinding::inflate
 
+    val viewModel: NewsViewModel by viewModels()
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun setupUI() {
         setSupportActionBar(binding.toolbar)
-
-        intent.getStringExtra(EXTRA_URL)?.let {
-            binding.webView.apply {
-                webViewClient = object : WebViewClient() {
+        viewModel.getCookie()
+        viewModel.cookie.observe(this) { cookie ->
+            intent.getStringExtra(EXTRA_URL)?.let { url ->
+                CookieManager.getInstance().apply {
+                    setAcceptCookie(true)
+                    setCookie(url, cookie)
+                    flush()
+                }
+                binding.webView.webViewClient = object : WebViewClient() {
                     override fun onPageFinished(view: WebView, url: String) {
                         supportActionBar?.apply {
                             setDisplayHomeAsUpEnabled(true)
@@ -26,7 +40,8 @@ class NewsActivity : BaseActivity<ActivityNewsBinding>() {
                         }
                     }
                 }
-                loadUrl(it)
+                binding.webView.loadUrl(url)
+                binding.webView.settings.javaScriptEnabled = true
             }
         }
     }
