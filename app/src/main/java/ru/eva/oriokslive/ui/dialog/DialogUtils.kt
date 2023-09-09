8b132e4d7fd7cc9f59dc5c0481559bae4ae75f78
Vladimir40a <altair08111994@yandex.ru>
2023-09-09 13:22:27 +0300

    profile fix

--- app/src/main/java/ru/eva/oriokslive/ui/dialog/DialogUtils.kt
+++ app/src/main/java/ru/eva/oriokslive/ui/dialog/DialogUtils.kt
@@ -10,11 +10,12 @@ fun showProfileDialog(context: Context, student: Student) {
     BottomSheetDialog(context).apply {
         val binding = DialogStudentBinding.inflate(layoutInflater)
         with(binding) {
-            tvCourse.text = context.getString(R.string.course, student.course)
-            tvId.text = context.getString(R.string.id, student.recordBookId)
-            tvSemester.text = context.getString(R.string.semester, student.semester)
+            tvDepartment2.text = context.getString(R.string.department, student.department)
             tvProfile.text = context.getString(R.string.training_profile, student.studyProfile)
+            tvCourse.text = context.getString(R.string.course, student.course)
+            tvSemester.text = context.getString(R.string.semester, student.semester)
             tvYear.text = context.getString(R.string.year, student.year)
+            tvId.text = context.getString(R.string.id, student.recordBookId)
         }
         setContentView(binding.root)
         show()
