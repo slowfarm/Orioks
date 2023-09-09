539a98cabfa0a8a298b6f73f0975875115baff36
Vladimir40a <altair08111994@yandex.ru>
2023-09-09 13:13:48 +0300

    fix empty list

--- app/src/main/java/ru/eva/oriokslive/domain/utils/Converters.kt
+++ app/src/main/java/ru/eva/oriokslive/domain/utils/Converters.kt
@@ -6,9 +6,12 @@ import com.google.gson.reflect.TypeToken
 
 class Converters {
     @TypeConverter
-    fun fromStringToList(string: String): List<String> =
+    fun fromStringToList(string: String): List<String> = if (string.isEmpty()) {
+        emptyList()
+    } else {
         Gson().fromJson(string, object : TypeToken<List<String>>() {}.type)
+    }
 
     @TypeConverter
-    fun fromListToString(list: List<String>): String = Gson().toJson(list)
+    fun fromListToString(list: List<String>?): String = list?.let { Gson().toJson(list) } ?: ""
 }
